8e6ecc126dcb5950a5bcaec6c1bce0a46800b382
Nelson Avila <nbrian.avila96@gmail.com>
2022-11-29 23:32:45 -0300

    endpont proyects estaba rompiendo todo

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import ticketsRoutes from "./routes/tickets.routes.js";
 import employeesRoutes from "./routes/employees.routes.js";
 import faltasRoutes from "./routes/faltas.routes.js";
 import horasRoutes from "./routes/horas.routes.js";
-import proyectsRoutes from "./routes/proyects.routes.js";
+//import proyectsRoutes from "./routes/proyects.routes.js";
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use("/api", ticketsRoutes);
 app.use("/api", employeesRoutes);
 app.use("/api", faltasRoutes);
 app.use("/api", horasRoutes);
-app.use("/api", proyectsRoutes);
+//app.use("/api", proyectsRoutes); // ESTA FALLANDO LUEGO LO VEO - Brian A.
 
 app.use((req, res, next) => {
     res.status(404).json({
