ce151166daa7a17ca7b323edcf87102807dac348
NCelano <ncelano@fi.uba.ar>
2022-12-01 10:50:11 -0300

    Debug createProject

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -11,20 +11,19 @@ export const getProjects = async (req, res) => {
 
 export const createProject = async (req, res) => {
     try {
-        const { nombre, fecha_inicio, fecha_fin, estado, prioridad, costo_acumulado, horas_estimada, horas_reales } = req.body;
+        const { nombre, fecha_inicio, fecha_fin, estado, prioridad, costo_acumulado, horas_estimadas, horas_reales } = req.body;
         const [result] = await pooldb.query('INSERT INTO tbl_proyecto SET ?', {
             nombre: nombre,
             fecha_inicio: fecha_inicio,
             fecha_fin: fecha_fin,
             estado: estado,
-            /*prioridad: prioridad,
+            prioridad: prioridad,
             costo_acumulado: costo_acumulado,
-            horas_estimada: horas_estimada,
+            horas_estimadas: horas_estimadas,
             horas_reales: horas_reales
-            */
         });
 
-        return res.status(200).json({ nombre, estado, id: result.insertId });
+        return res.status(200).json({ nombre, fecha_inicio, fecha_fin, estado, prioridad, costo_acumulado, horas_estimadas, horas_reales, id: result.insertId });
     } catch (error) {
         return res.status(500).json({ message: "Something goes wrong" });
     }
