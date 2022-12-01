9f9295764d58cad3574e52d8d78a25df0418678c
BrunoPasca <42937086+BrunoPasca@users.noreply.github.com>
2022-12-01 14:13:00 -0300

    adds logs for debugging

--- src/controllers/horas.controller.js
+++ src/controllers/horas.controller.js
@@ -1,31 +1,31 @@
 import { pooldb } from "../db.js";
 
 export const getHoras = async (req, res) => {
-    try {
-      const [rows] = await pooldb.query("SELECT * FROM tbl_horas");
-      res.json(rows);
-    } catch (error) {
-      const loQueEnvian = req.body;
-      res.status(500).send({loQueEnvian});
-    }
+  try {
+    const [rows] = await pooldb.query("SELECT * FROM tbl_horas");
+    res.json(rows);
+  } catch (error) {
+    const loQueEnvian = req.body;
+    res.status(500).send({ loQueEnvian });
+  }
 };
 
 export const getHora = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const [rows] = await pooldb.query("SELECT * FROM tbl_horas WHERE id = ?", [
-        id,
-      ]);
-  
-      if (rows.length <= 0) {
-        return res.status(404).json({ message: "Hora not found" });
-      }
-  
-      res.json(rows[0]);
-    } catch (error) {
-      const loQueEnvian = req.body;
-      res.status(500).send({loQueEnvian});
+  try {
+    const { id } = req.params;
+    const [rows] = await pooldb.query("SELECT * FROM tbl_horas WHERE id = ?", [
+      id,
+    ]);
+
+    if (rows.length <= 0) {
+      return res.status(404).json({ message: "Hora not found" });
     }
+
+    res.json(rows[0]);
+  } catch (error) {
+    const loQueEnvian = req.body;
+    res.status(500).send({ loQueEnvian });
+  }
 };
 
 export const getHoraEmpleado = async (req, res) => {
@@ -42,56 +42,58 @@ export const getHoraEmpleado = async (req, res) => {
     res.json(rows[0]);
   } catch (error) {
     const loQueEnvian = req.body;
-    res.status(500).send({loQueEnvian});
+    res.status(500).send({ loQueEnvian });
   }
 };
 
 export const deleteHora = async (req, res) => {
-    try {
-      const { id } = req.params.id;
-      const [rows] = await pooldb.query("DELETE FROM tbl_horas WHERE id = ?", [id]);
-  
-      if (rows.affectedRows <= 0) {
-        return res.status(404).json({ message: "Hora not found" });
-      }
-  
-      res.sendStatus(204);
-    } catch (error) {
-      const loQueEnvian = req.body;
-      res.status(500).send({loQueEnvian});
+  try {
+    const { id } = req.params.id;
+    const [rows] = await pooldb.query("DELETE FROM tbl_horas WHERE id = ?", [id]);
+
+    if (rows.affectedRows <= 0) {
+      return res.status(404).json({ message: "Hora not found" });
     }
+
+    res.sendStatus(204);
+  } catch (error) {
+    const loQueEnvian = req.body;
+    res.status(500).send({ loQueEnvian });
+  }
 };
 
 export const updateHoras = async (req, res) => {
-	let { id } = req.params;
-	let { id_tarea, cant_horas } = req.body;
+  let { id } = req.params;
+  let { id_tarea, cant_horas } = req.body;
 
-	let [rows] = await pooldb.query(
-		"update tbl_horas set id_tarea = ?, cant_horas = ?  where id = ?",
-		[id_tarea, cant_horas, id]
-	);
+  let [rows] = await pooldb.query(
+    "update tbl_horas set id_tarea = ?, cant_horas = ?  where id = ?",
+    [id_tarea, cant_horas, id]
+  );
 
-	if (rows.affectedRows < 1)
-		return res.status(404).json({
-			message: "Hora not found",
-		});
+  if (rows.affectedRows < 1)
+    return res.status(404).json({
+      message: "Hora not found",
+    });
 
-	let [result] = await pooldb.query("select * from tbl_horas where id = ?", [id]);
+  let [result] = await pooldb.query("select * from tbl_horas where id = ?", [id]);
 
-	res.json(result[0]);
+  res.json(result[0]);
 };
 
 export const createHora = async (req, res) => {
-    try {
-        let { legajo, id_tarea, cant, fecha, estado,  extra } = req.body;
+  console.log(req.body);
+  try {
+    let { legajo, id_tarea, cant, fecha, estado, extra } = req.body;
 
-        let [rows] = await pooldb.query(
-          "INSERT INTO tbl_horas (legajo_empleado, id_tarea, cant_horas, fecha, estado, extra) VALUES (?, ?, ?, ?, ?, ?)",
-          [legajo, id_tarea, cant, fecha, estado ,extra]
-        );
-        res.send({ id: rows.insertId, legajo, id_tarea, cant, fecha, estado, extra });
-      } catch (error) {
-        const loQueEnvian = req.body;
-        res.status(500).send({loQueEnvian});
-      }
+    let [rows] = await pooldb.query(
+      "INSERT INTO tbl_horas (legajo_empleado, id_tarea, cant_horas, fecha, estado, extra) VALUES (?, ?, ?, ?, ?, ?)",
+      [legajo, id_tarea, cant, fecha, estado, extra]
+    );
+    res.send({ id: rows.insertId, legajo, id_tarea, cant, fecha, estado, extra });
+  } catch (error) {
+    console.log(error);
+    const loQueEnvian = req.body;
+    res.status(500).send({ loQueEnvian });
+  }
 };
