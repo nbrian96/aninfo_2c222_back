b72027353bce93a27a7877cda47e2b801da79ce2
NCelano <ncelano@fi.uba.ar>
2022-12-04 18:19:21 -0300

    Debug deleteProject

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -48,14 +48,14 @@ export const deleteProject = async (req, res) => {
     try {
 
         let [tareas_pendientes] = await pooldb.query("SELECT * FROM tbl_tarea WHERE id_proyecto = ? ", [req.params.id]);
-        if(tareas_pendientes.affectedRows > 0){
+        if(tareas_pendientes.lenght > 0){
             return res.status(400).json({
                 message: "El proyecto aun tiene tareas sin terminar"
             })
         }
-
+        
         let [rows] = await pooldb.query("DELETE FROM tbl_proyecto WHERE id = ?", [req.params.id]);
-        if (rows.affectedRows <= 0)
+        if (rows.length < 1)
             return res.status(404).json({
                 message: "Project not found",
             });
