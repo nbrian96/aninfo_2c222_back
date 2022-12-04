f31332e30c79e73702d84eb306a437554952d158
Nelson Avila <33665807+nbrian96@users.noreply.github.com>
2022-12-04 14:52:56 -0300

    Update tbl_productos_y_versiones.routes.js

--- src/routes/tbl_productos_y_versiones.routes.js
+++ src/routes/tbl_productos_y_versiones.routes.js
@@ -1,1 +1,25 @@
+import { Router } from "express";
 
+import {
+
+	getProdVersions,	getProdVersion,
+
+	createProdVersion,getProd,
+
+	deteleProdVersion,
+
+} from "../controllers/tbl_productos_y_versiones.controller.js";
+
+const router = Router();
+
+router.get("/prodversions", getProdVersions);
+
+router.get("/prodversions/:id", getProdVersion);
+
+router.post("/prodversions", createProdVersion);
+
+router.get("/prodversions/producto/:id", getProd);
+
+router.delete("/prodversions/:id", deteleProdVersion);
+
+export default router;
