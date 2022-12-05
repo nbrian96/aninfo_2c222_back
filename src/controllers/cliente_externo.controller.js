2ae98b223251fe1adf2e15dcc01e18171141b8f5
Nelson Avila <nbrian.avila96@gmail.com>
2022-12-05 19:45:05 -0300

    cambio get cliente ext

--- src/controllers/cliente_externo.controller.js
+++ src/controllers/cliente_externo.controller.js
@@ -1,8 +1,8 @@
 const url_api_clientes = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/clientes-psa/1.0.0/m/api/clientes";
 
-export const getClientsExt = async (req, res) => {
+export const getClientsExt = (req, res) => {
     try {
-        fetch(url_api_clientes).then((respuesta) => {return res.json(respuesta);})
+        fetch(url_api_clientes).then((respuesta) => {return respuesta.json();})
     } catch (error) {
         res.status(500).send({ error });
     }
