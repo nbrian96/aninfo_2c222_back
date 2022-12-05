de15414143e4151037ffea8da932369345b1742e
Nelson Avila <nbrian.avila96@gmail.com>
2022-12-05 18:57:38 -0300

    Get segun producto en el endpoint tbl_cliente_producto

--- src/routes/tbl_cliente_producto.routes.js
+++ src/routes/tbl_cliente_producto.routes.js
@@ -6,6 +6,7 @@ import {
 	createClienteProd,
 	updateClienteProd,
 	getProdWithCliente,
+	getClienteWithProd,
 } from "../controllers/tbl_cliente_producto.controller.js";
 
 const router = Router();
@@ -15,6 +16,8 @@ router.get("/cliente_prod", getClienteProds);
 router.get("/cliente_prod/:id", getClienteProd);
 
 router.get("/cliente_prod/cliente/:id", getProdWithCliente);
+
+router.get("/cliente_prod/producto/:id", getClienteWithProd);
 
 router.delete("/cliente_prod/:id", deleteClienteProd);
 
